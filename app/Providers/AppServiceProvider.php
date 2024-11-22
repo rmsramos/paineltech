7381ce86861b05476e8f1ff607a439a984f90e91
Rômulo Ramos <rmsramos@gmail.com>
2024-11-22 15:08:56 -0300

    create and setup new filament theme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerNewIcons();
+    }
+
+    public function registerNewIcons()
+    {
+        FilamentIcon::register([
+            'panels::topbar.open-database-notifications-button' => 'phosphor-bell-ringing-duotone',
+            'panels::pages.dashboard.navigation-item'           => 'phosphor-house-duotone',
+            'panels::theme-switcher.light-button'               => 'phosphor-sun-horizon-duotone',
+            'panels::theme-switcher.dark-button'                => 'phosphor-moon-stars-duotone',
+            'panels::theme-switcher.system-button'              => 'phosphor-monitor-duotone',
+            'panels::user-menu.profile-item'                    => 'phosphor-user-circle-duotone',
+            'panels::user-menu.logout-button'                   => 'phosphor-sign-out-duotone',
+            'panels::widgets.account.logout-button'             => 'phosphor-sign-out-duotone',
+            'tables::actions.filter'                            => 'phosphor-funnel-duotone',
+            'tables::actions.toggle-columns'                    => 'phosphor-columns-duotone',
+        ]);
     }
 }
